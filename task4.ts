a970e7e2afa841864049adf2377ba72bc558e1de
pirnataly <piroznatal@gmail.com>
2024-11-03 22:42:59 +0300

    add a few methods

--- task4.ts
+++ task4.ts
@@ -1,19 +1,44 @@
-import { goods,Categories,OrderState } from './data'
+import {goods, Categories, OrderState} from './data'
 
-class Product {
+interface ProductInterface {
   id: string;
   name: string;
   url: string;
   description: string;
   category: Categories;
-  currencyCode: "EUR";
+  currencyCode: string;
   centAmount: number;
+}
+
+class Product implements ProductInterface {
+  id: string;
+  name: string;
+  url: string;
+  description: string;
+  category: Categories;
+  currencyCode: 'EUR';
+  centAmount: number;
+
+  constructor(product: ProductInterface) {
+    this.id = product.id;
+    this.category = product.category;
+    this.name=product.name;
+    this.url=product.url;
+    this.description=product.description;
+    this.currencyCode='EUR';
+    this.centAmount=product.centAmount
+  }
+
+  addToCart(id) {
+
+  }
 }
 
 class Order {
   id: string;
   content: Product[];
   orderState: OrderState;
+
   constructor(content: Product[]) {
     this.id = String(Math.floor(1 + Math.random() * (100000 + 1 - 1)));
     this.content = content;
@@ -23,23 +48,59 @@ class Order {
 
 class Cart {
   id: string;
-  productItems: Product[];
-  totalPrice: string;
-  currencyCode:"EUR";
+  static productItems: Product[] = [];
+  static totalPrice: number = Cart.productItems.reduce(function(sum:number, currentItem:ProductInterface) {
+    return sum + currentItem.centAmount;
+  },0)
+  currencyCode: "EUR";
+
+
+  static showCart() {
+    return Cart.productItems
+  }
+
 }
 
 class OrderManager {
   static orders: [Order];
+
   static showOrders() {
     return OrderManager.orders;
   }
+
 }
 
 
 class ProductManager {
-  static products: [Product];
-  static showProducts() {
+  static products: Product[]=[];
+
+  static showAllProducts() {
     return ProductManager.products;
+  }
+  static addToCart(id:string) {
+    ProductManager.products.forEach((product)=>{
+      if(product.id===id){
+        const chosenProduct = product;
+        Cart.productItems.push(chosenProduct );
+      }
+    })
+
   }
 }
 
+console.log('Make goods from file data.ts')
+goods.forEach(product => {
+  ProductManager.products.push(new Product(product))
+});
+
+console.log('Show all goods',ProductManager.showAllProducts());
+
+console.log('Add to cart the item with id=2');
+ProductManager.addToCart('2')
+
+console.log('Check the cart',Cart.showCart());
+
+console.log('Add to cart the item with id=5');
+ProductManager.addToCart('5')
+
+console.log('Check the cart',Cart.showCart());
